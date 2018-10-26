7668854c58ecbbe4589468d3b11625893ff36852
Josh Ghiloni <jghiloni@pivotal.io>
2018-10-26 15:37:52 -0600

    Even more

---
+++ integration-tests/ci/tasks/delete-credhub.sh
@@ -0,0 +1,15 @@
+#!/bin/bash 
+
+set -eu
+
+BASEDIR=$(pwd)
+
+mv bbl-cli/bbl*linux* /usr/local/bin/bbl
+mv bosh-cli/bosh*linux* /usr/local/bin/bosh
+
+chmod +x /usr/local/bin/*
+
+cd bbl-state
+
+eval "$(bbl print-env)"
+
--- integration-tests/ci/tasks/deploy-credhub.sh
+++ integration-tests/ci/tasks/deploy-credhub.sh
@@ -11,7 +11,14 @@ chmod +x /usr/local/bin/*
 
 cd bbl-state
 
-bbl print-env
-#eval "$(bbl print-env)"
+eval "$(bbl print-env)"
+
+for release in $(find ${BASEDIR} -name '*-bosh-release' -type d); do
+    bosh upload-release --sha1="$(cat ${release}/sha1)" --version="$(cat ${release}/version)" "$(cat ${release}/url)"
+done
+
+for stemcell in $(find ${BASEDIR} -name '*-stemcell' -type d); do
+    bosh upload-stemcell --sha1="$(cat ${stemcell}/sha1)" --version="$(cat ${stemcell}/version)" "$(cat ${stemcell}/url)"
+done
 
 #bosh -d credhub deploy ${BASEDIR}/source/integration-tests/manifest/credhub.yml
